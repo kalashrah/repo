96151a12a8cbaa32eee84e0e3e764f5a06f5f3ce
kalashrah <kalashrah@hotmail.com>
2020-10-02 04:53:27 +0300

    .
    .

--- run.sh
+++ run.sh
@@ -2,6 +2,12 @@
 git pull
 rm ./Packages
 touch ./Packages
-./dpkg-scanpackages deb ./deb > Packages
+dpkg-scanpackages deb ./deb > Packages
 bzip2 -c9 Packages > Packages.bz2
 echo "Updated Packages"
+
+
+# rm ./Packages
+# ./dpkg-scanpackages deb / > Packages
+# rm ./Packages.bz2
+# bzip2 -fks Packages
